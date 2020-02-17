27b4a4ce643d23982453e3ff368f7cc91736d1cf
Rara Technologies <raratechnologies2018@gmail.com>
2020-02-17 14:59:39 -0500

    Added alice code

---
+++ Java/lab1.java
@@ -0,0 +1,21 @@
+
+public class Lab1 {
+    /*
+    public static void main(String[] args)
+    {
+
+
+        system.out.println("This is my first message");
+
+        System.out.Println(This is my second message);
+
+        System.Out.println("This is my third message")
+
+        System.println("This is another message")
+
+          System.printLn("This is another message")
+}
+
+     */
+
+}
