1ad1c7763dc5cebd45d10e81cf525dba2c181493
Rara Technologies <raratechnologies2018@gmail.com>
2020-02-24 10:42:55 -0500

    added java code

---
+++ Chapter4/variable.java
@@ -0,0 +1,22 @@
+package chapter4;
+
+public class variable {
+
+	public static void main(String[] args) {
+	
+		add(2,5);
+		sub(4,2);
+		int mynumber=128;
+	}
+	
+	public static int add(int num1,int num2) {
+		int r=num1+num2;
+		return r;
+	}
+	
+	public static int sub(int num1,int num2) {
+		int r=num1-num2;
+		return r;
+	}
+
+}
