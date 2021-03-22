e7569b5668a334a2bf9580d91509ace4e7beedd7
Shiva Sharma <ss12@umbc.edu>
2021-03-22 09:34:03 -0400

    Create ifexamples.java

---
+++ Chapter5/ifexamples.java
@@ -0,0 +1,14 @@
+
+
+int time = 20;
+if (time < 18) {
+  System.out.println("Good day.");
+} else {
+  System.out.println("Good evening.");
+}
+
+#Short Hand If...Else (Ternary Operator)
+
+int time = 20;
+String result = (time < 18) ? "Good day." : "Good evening.";
+System.out.println(result);
